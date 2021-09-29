27da0353ba81cb1944d5b9126627f3fcd07b749a
Mauricio Zapata <premier8383.com>
2021-09-28 22:05:04 -0400

    Initial commit

---
+++ src/main/java/com/pi/pi4led/controller/LedController.java
@@ -0,0 +1,27 @@
+package com.pi.pi4led.controller;
+
+import com.pi4j.io.gpio.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LedController {
+
+    private static GpioPinDigitalOutput pin;
+
+    @RequestMapping("/")
+    public String greeting() {
+        return "Hello world";
+    }
+
+    @RequestMapping("/light")
+    public String light() {
+        if (pin == null) {
+            GpioController gpio = GpioFactory.getInstance();
+            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_17, "MyLED", PinState.LOW);
+        }
+
+        pin.toggle();
+        return "OK";
+    }
+}
